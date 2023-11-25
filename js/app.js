0b24d1e87f7d8a0ee932d73b53172aa4d1d01a49
Adarsh Sahu <adarshsahu1077@gmail.com>
2023-11-25 23:47:27 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const endDate = "23 November 2023 1:17 PM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+
+function clock(){
+    const end = new Date(endDate)
+    const cur = new Date()
+    const dif = (end - cur)/1000;
+    if(dif < 0){
+        return
+    }
+    inputs[0].value = Math.floor(dif / 3600 / 24);
+    inputs[1].value = Math.floor((dif / 3600) % 24);
+    inputs[2].value = Math.floor((dif / 60) % 60);
+    inputs[3].value = Math.floor(dif % 60);
+}
+
+clock()
+
+setInterval(()=>{clock()}, 1000)
+
+// or
+// function f(){clock()}
+
